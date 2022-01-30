00514223c8a2468fa495ffd767555899bb39e954
SergeLipatnikov <qiu005b@gmail.com>
2022-01-31 00:43:45 +0300

    les1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import './Styles/mess_style.css';
+import { Message } from './Components/Message';
+
+
+const myText = 'Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="mes">
+        <Message text={myText} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
